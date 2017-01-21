a35c79be8760f8f52fd388d8f2237c51766d2710
aQaTL <mmsoltys@outlook.com>
2017-01-21 11:19:11 +0100

    Added more verbose info

--- src/com/aqatl/podcastget/PodcastGet.py
+++ src/com/aqatl/podcastget/PodcastGet.py
@@ -6,6 +6,7 @@ config = configparser.ConfigParser()
 config.read('../../../../config.ini')
 
 for item in config["PODCASTS"].items():
+    print("Searching for new " + item[0] + " episode")
     podcastFeed = feedparser.parse(item[1])
     epFeed = podcastFeed.entries[0]
     epUrl = epFeed.enclosures[0]["href"]
@@ -21,3 +22,5 @@ for item in config["PODCASTS"].items():
         print("Started downloading " + epFilename + " from " + item[0] + "\n")
         os.system("wget " + epUrl + " -O " + epFilename)
         print(epFilename + " has been downloaded into " + folderPath)
+    else:
+        print("Nothing new here")
